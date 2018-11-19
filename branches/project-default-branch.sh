992fd4f0f909e2a803d94a932bb10796d8772587
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 22:25:47 +0100

    Update branches/project-default-branch.sh

--- branches/project-default-branch.sh
+++ branches/project-default-branch.sh
@@ -23,9 +23,8 @@ gitlab_project_default_branch () {
 
     local default_branch="";
 
-    # Look which is the default branch
+    # Look which is the default branch from project record
     default_branch=$(json_value ".default_branch" "$(gitlab_project_api)");
-    #default_branch=`echo $(gitlab_project_api) | jq --raw-output '.default_branch'`;
 
     export CI_PROJECT_DEFAUL_BRANCH=$default_branch
     echo $CI_PROJECT_DEFAUL_BRANCH;
